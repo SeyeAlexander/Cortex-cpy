373048e893e3bef7c9a62fb345aef842f0f57b88
Design engr <ojubanirealex@gmail.com>
2022-12-29 02:58:18 +0100

    made more components responsive on mobile screens

--- src/components/Contribute.tsx
+++ src/components/Contribute.tsx
@@ -11,20 +11,25 @@ const Contribute = () => {
           align='flex-start'
           bg='linear-gradient(74deg, #ececec 0%, #e1e1e1 58%)'
           w='full'
-          h='300px'
-          p='50px'
-          borderRadius='20px'
+          h={{ xl: "300px" }}
+          p={{ base: "17px", xl: "50px" }}
+          borderRadius={{ base: "10px", xl: "20px" }}
         >
-          <Box mb='55px' lineHeight='40px'>
-            <Text fontFamily='Aeonik Bold' fontSize='33px' color='green'>
+          <Box mb='55px' lineHeight={{ base: "35px", xl: "40px" }}>
+            <Text fontFamily='Aeonik Bold' fontSize={{ base: "25px", xl: "33px" }} color='green'>
               Understanding
             </Text>
-            <Text fontFamily='Aeonik Bold' fontSize='33px'>
+            <Text fontFamily='Aeonik Bold' fontSize={{ base: "25px", xl: "33px" }}>
               Cortex Tokenomics
             </Text>
           </Box>
 
-          <Button colorScheme='green' w='220px' h='50px' borderRadius='10px'>
+          <Button
+            colorScheme='green'
+            w={{ base: "full", xl: "220px" }}
+            h='50px'
+            borderRadius='10px'
+          >
             Read Whitepaper
           </Button>
         </VStack>
@@ -32,28 +37,46 @@ const Contribute = () => {
         <Flex
           w='full'
           position='relative'
-          h='300px'
-          borderRadius='20px'
+          h={{ xl: "300px" }}
+          borderRadius={{ base: "10px", xl: "20px" }}
           bg='linear-gradient(108deg, #191f2d 0%, #363f53 51%)'
         >
-          <VStack align='flex-start' p='50px'>
-            <Box mb='55px' lineHeight='40px'>
-              <Text fontFamily='Aeonik Bold' fontSize='33px' color='#18A672'>
+          <VStack align='flex-start' p={{ base: "17px", xl: "50px" }} w='full'>
+            <Box mb='55px' lineHeight={{ base: "35px", xl: "40px" }}>
+              <Text
+                fontFamily='Aeonik Bold'
+                fontSize={{ base: "25px", xl: "33px" }}
+                color='#18A672'
+              >
                 Contribute
               </Text>
 
               <HStack>
-                <Text fontFamily='Aeonik Bold' fontSize='33px' color='white'>
+                <Text
+                  fontFamily='Aeonik Bold'
+                  fontSize={{ base: "25px", xl: "33px" }}
+                  color='white'
+                >
                   to the
                 </Text>
                 <Image src={icon1} w='30px' />
-                <Text fontFamily='Aeonik Bold' fontSize='33px' color='white'>
+                <Text
+                  fontFamily='Aeonik Bold'
+                  fontSize={{ base: "25px", xl: "33px" }}
+                  color='white'
+                >
                   DAO
                 </Text>
               </HStack>
             </Box>
 
-            <Button colorScheme='green' w='220px' h='50px' borderRadius='10px'>
+            <Button
+              colorScheme='green'
+              w={{ base: "full", xl: "220px" }}
+              h='50px'
+              borderRadius='10px'
+              zIndex={1}
+            >
               Contribute
             </Button>
           </VStack>
